4f99636709ad59f2a0e702c7321dbd5b71decf5b
Renato Rocha <renatomrocha93@gmail.com>
2019-01-22 15:12:46 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+(function () {
+    'use strict';
+    
+    angular.module('NarrowOptionsApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('foundItems',FoundItemsDirective)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    
+
+    function FoundItemsDirective(){
+      var ddo = {
+        templateUrl: '../loader/itemsloader.html',
+        scope: {
+          items: '<',
+          myTitle: '@title',
+          onRemove: '&',
+        },
+      controller: NarrowItDownDirectiveController,
+      controllerAs: 'dirctrl',
+      bindToController: true
+      //transclude: true
+      };
+
+      return ddo;
+    }
+
+
+    function NarrowItDownDirectiveController() {
+        var ctrl = this;
+
+    }
+   
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+      var ctrl = this;
+      ctrl.searchString = "";
+
+      ctrl.title = "Filtered List";
+
+      ctrl.logInformation = function(){
+        ctrl.found = [];
+        // console.log("SearchString is: " + ctrl.searchString);
+      var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchString).then(function(response){
+        ctrl.found = response;
+        // console.log(ctrl.found[0]);
+        ctrl.title = "Filtered List " + "(" + ctrl.searchString + ")";
+      });
+    
+      };
+
+      ctrl.removeItem = function(itemIndex){
+        MenuSearchService.removeItem(itemIndex);
+
+      }
+
+    }
+    
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+      var service = this;
+      var names = [];
+      service.getMatchedMenuItems = function(searchString){
+        names = [];
+        return $http({
+            method: 'GET',
+            url:(ApiBasePath + "/menu_items.json"),
+        }).then(function(response){
+        
+          // console.log("Search String in service: " + searchString);
+          for (var i in response.data.menu_items){ 
+            if(response.data.menu_items[i].name.toLowerCase().includes(searchString)){
+              // console.log("This verifies: " + response.data.menu_items[i].name);
+              names.push(response.data.menu_items[i].name);
+            }
+         }
+         return names;
+        });
+      
+    
+    }
+    
+    service.removeItem = function(itemIndex){
+      names.splice(itemIndex,1);
+    };
+
+
+    }
+    })();
+    
